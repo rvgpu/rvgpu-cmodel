ed6d78ecaaeb860612d5a0f1122876d05b13d6c3
Qi Hangkong <qihangkong@outlook.com>
2023-04-25 17:56:56 +0800

    feat: add interface of drmGetDevice2

--- libdrm/libdrm.c
+++ libdrm/libdrm.c
@@ -22,8 +22,47 @@
  */
 
 #include <xf86drm.h>
+#include <stdlib.h>
+#include <string.h>
 
 int drmOpen(const char *name, const char *busid)
 {
     return 0;
+}
+
+int drmGetDevices2(uint32_t flags, drmDevicePtr devices[], int max_devices)
+{
+    drmDevicePtr rvgsim_device;
+    if (max_devices < 1) {
+        return 0;
+    }
+
+    rvgsim_device = malloc(sizeof(drmDevice));
+
+    rvgsim_device->nodes = malloc(sizeof(char *) * DRM_NODE_MAX);
+    rvgsim_device->nodes[DRM_NODE_PRIMARY] = "rvgsim-primary";
+    rvgsim_device->nodes[DRM_NODE_CONTROL] = "rvgsim-control";
+    rvgsim_device->nodes[DRM_NODE_RENDER] = "rvgsim-render";
+
+    /* set to DRM_NODE_RENDER */
+    rvgsim_device->available_nodes = 1 << DRM_NODE_RENDER;
+    rvgsim_device->bustype = DRM_BUS_PCI;
+
+    /* use PCI device here */
+    rvgsim_device->businfo.pci = malloc(sizeof(drmPciBusInfo));
+    memset(rvgsim_device->businfo.pci, 0x0, sizeof(drmPciBusInfo));
+    rvgsim_device->deviceinfo.pci = malloc(sizeof(drmPciDeviceInfo));
+    memset(rvgsim_device->deviceinfo.pci, 0x0, sizeof(drmPciDeviceInfo));
+    rvgsim_device->deviceinfo.pci->vendor_id = 0x16c3;
+    rvgsim_device->deviceinfo.pci->device_id = 0x0001;
+
+    devices[0] = rvgsim_device;
+
+    // return the number of devices
+    return 1;
+}
+
+int drmGetDevice2(int fd, uint32_t flags, drmDevicePtr *device)
+{
+    drmGetDevices2(0, device, 1);
 }
