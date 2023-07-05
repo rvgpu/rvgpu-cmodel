77ce1a5d0ce33886e1220b9be8c2cb37d2eddabd
max <max@sietium.com>
2023-07-05 11:25:55 +0800

    test: use TEST_F to test sw inst
    Signed-off-by: max <max@sietium.com>

--- test/unit/ut_insns.hpp
+++ test/unit/ut_insns.hpp
@@ -2,14 +2,19 @@
 
 #include "sm/compute_unit.h"
 
+#define STACK_SIZE  2000
+
 class ut_insns : public ::testing::Test {
 protected:
     void SetUp() override {
         p = new class compute_unit();
+        sp = (uint32_t*) malloc(STACK_SIZE * sizeof (uint32_t));
+        SetspHighToMMU();
     }
 
     void TearDown() override {
         delete p;
+        free(sp);
     }
 
     void LoadInst() {
@@ -22,10 +27,15 @@ protected:
         next_pc = (uint64_t)((uint32_t)next_pc - (uint32_t)pc) + pc;
     }
 
+    void SetspHighToMMU() {
+        uint64_t sp_high = (uint64_t)sp & (0xffffffffLU << 32U);
+        MMU.set_base_addr(sp_high);
+    }
     compute_unit *p;
     std::vector<uint32_t> insts;
 
     uint64_t pc;
     uint64_t next_pc;
     insn_fetch_t fetch;
+    uint32_t *sp;
 };
--- test/unit/ut_sw.cpp
+++ test/unit/ut_sw.cpp
@@ -1,25 +1,16 @@
 #include <gtest/gtest.h>
 #include "sm/compute_unit.h"
+#include "ut_insns.hpp"
 
-TEST(test_inst, should_decode_and_execute_sw_correctly) {
-    auto p = new class compute_unit();
-    auto mem = (uint32_t*)calloc(1200, sizeof(uint32_t));
+TEST_F(ut_insns, should_decode_and_execute_sw_correctly) {
     //sw bits: 0x48112623
-    mem[0] = 0x48112623;
-
-    auto sp = (uint32_t*)malloc(sizeof(uint32_t));
-    uint32_t sp_high = (uint64_t)sp & (0xffffffffLU << 32U);
+    insts.push_back(0x48112623);
     uint32_t sp_low = (uint64_t)sp & 0xffffffff;
+    LoadInst();
 
-    MMU.set_base_addr(sp_high);
-
-    auto pc = (uint64_t)(&mem[0]);
-    auto fetch = p->load_insn(pc);
     WRITE_REG(fetch.insn.rs1(), sp_low);
     WRITE_REG(fetch.insn.rs2(), 333);
 
-    uint64_t next_pc = p->execute_insn(pc, fetch);
-
-    EXPECT_EQ(next_pc, pc + 4);
+    ExecuateInst();
     EXPECT_EQ(*(uint32_t*)((uint64_t)sp + 1164), 333);
 }
