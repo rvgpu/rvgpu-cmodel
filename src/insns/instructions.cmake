fa47cba83e7f65b9f53087e2ecd0979ab7b5ce03
Qi Hangkong <qihangkong@outlook.com>
2023-07-05 11:47:34 +0800

    feat: add new instruction add

--- src/insns/instructions.cmake
+++ src/insns/instructions.cmake
@@ -1,4 +1,5 @@
 # Need add instruction here
+add_instruction(add)
 add_instruction(addi)
 add_instruction(auipc)
 add_instruction(lui)
---
+++ test/unit/ut_add.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "sm/compute_unit.h"
+#include "ut_insns.hpp"
+
+#define CHECK_ADD(sum, a, b) do { \
+        WRITE_REG(fetch.insn.rs1(), a); \
+        WRITE_REG(fetch.insn.rs2(), b); \
+        ExecuateInst();           \
+        result = READ_REG(fetch.insn.rd()); \
+        EXPECT_EQ(result, sum); \
+    } while(0)
+
+TEST_F(ut_insns, should_decode_and_execute_add_correctly) {
+    // 0x00b785b3 # add a1, a5, a1
+    insts.push_back(0x00b785b3);
+    uint32_t result;
+    LoadInst();
+
+    CHECK_ADD(3, 1, 2);
+    CHECK_ADD(1, -1, 2);
+    CHECK_ADD(-1, 1, -2);
+    CHECK_ADD(0, 1, -1);
+}
