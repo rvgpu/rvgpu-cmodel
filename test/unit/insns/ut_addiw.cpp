37055eeb3066a0dfe514c8541133448e7b229e4f
max <max@sietium.com>
2023-07-07 16:59:11 +0800

    test: add test for sext.w which is pseudoinstruction of addiw rd, rs, 0
    Signed-off-by: max <max@sietium.com>

--- test/unit/insns/ut_addiw.cpp
+++ test/unit/insns/ut_addiw.cpp
@@ -21,3 +21,15 @@ TEST_F(ut_rv64_insns, should_decode_and_execute_rv64i_add_correctly) {
     CHECK_ADD(12, 1);
     CHECK_ADD(-12, 1);    
 }
+
+TEST_F(ut_rv64_insns, execute_pseudoinstruction_sextw_correctly) {
+    //bits: 0x0006869b  #sext.w  a3, a3  pseudoinstruction of addiw rd, rs, 0
+    // writes the sign-extension of the lower 32 bits of register rs1 into register rd
+    insts.push_back(0x0006869b);
+    LoadInst();
+    WRITE_REG(fetch.insn.rs1(), 0x111180000001);
+    ExecuateInst();
+    auto res = READ_REG(fetch.insn.rs1());
+
+    EXPECT_EQ(res, 0xffffffff80000001);
+}
