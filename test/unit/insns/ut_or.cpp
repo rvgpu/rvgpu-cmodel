6ab653c900911edf683c662a1175462961ccfc48
max <max@sietium.com>
2023-07-08 15:32:36 +0800

    feat: add or instruction
    Signed-off-by: max <max@sietium.com>

---
+++ test/unit/insns/ut_or.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "ut_insns.hpp"
+
+TEST_F(ut_rv64_insns, should_decode_and_execute_or_correctly) {
+    //bits: 0x00a5e533     #or  a0, a1, a0
+    insts.push_back(0x00a5e533);
+    LoadInst();
+
+    WRITE_REG(fetch.insn.rs1(), 4);
+    WRITE_REG(fetch.insn.rs2(), 1);
+    ExecuateInst();
+    auto res = READ_REG(fetch.insn.rd());
+    EXPECT_EQ(res, 5);
+}
