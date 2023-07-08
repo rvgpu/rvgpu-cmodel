1885e303d2ebd861eeca7c9fa793f339b0e0f02d
max <max@sietium.com>
2023-07-08 14:57:27 +0800

    feat: add sd instruction
    Signed-off-by: max <max@sietium.com>

---
+++ test/unit/insns/ut_sd.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "sm/compute_unit.h"
+#include "ut_insns.hpp"
+
+TEST_F(ut_rv64_insns, decode_and_execute_sd_should_write_data_to_memory_address) {
+    //bits: 0x00a13c23   sd  a0, 24(sp)
+    insts.push_back(0x00a13c23);
+    LoadInst();
+    WRITE_REG(fetch.insn.rs2(), 0x1000100010001000);
+    WRITE_REG(fetch.insn.rs1(), (uint64_t)sp);
+
+    ExecuateInst();
+
+    EXPECT_EQ(*(uint64_t*)((uint64_t)sp + 24), 0x1000100010001000);
+}
