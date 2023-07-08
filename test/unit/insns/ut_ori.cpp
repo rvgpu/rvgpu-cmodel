28edf18fc70d72786773047e9fa44772e8bc6bde
max <max@sietium.com>
2023-07-08 15:22:32 +0800

    feat: add ori intstruction
    Signed-off-by: max <max@sietium.com>

---
+++ test/unit/insns/ut_ori.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "sm/compute_unit.h"
+#include "ut_insns.hpp"
+
+TEST_F(ut_rv64_insns, decode_and_execute_ori) {
+    //bits: 0x0044e513     #ori a0, s1, 4
+    insts.push_back(0x0044e513);
+    LoadInst();
+    WRITE_REG(fetch.insn.rs1(), 1);
+
+    ExecuateInst();
+
+    auto res = READ_REG(fetch.insn.rd());
+    EXPECT_EQ(res, 5);
+}
