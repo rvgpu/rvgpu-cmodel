233eb19bae3d80e90ae3faf20715fda102c1e814
lewis <lewis@sietium.com>
2023-07-24 20:40:58 +0800

    chore: change test names

--- test/unit/insns/ut_slliw.cpp
+++ test/unit/insns/ut_slliw.cpp
@@ -2,7 +2,7 @@
 #include "sm/compute_unit.h"
 #include "ut_insns.hpp"
 
-TEST_F(ut_rv64_insns, decode_and_execute_rv64i_swlli) {
+TEST_F(ut_rv64_insns, decode_and_execute_rv64i_slliw) {
     //bits 0x00169c1b  #slliw   s8, a3, 1
     //operate on 32-bit values and produce signed 32-bit results
     insts.push_back(0x00169c1b);
