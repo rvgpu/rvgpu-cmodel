00b3627f9fabc774ac5ba0abb1608f79ad0c0835
Qi Hangkong <qihangkong@outlook.com>
2023-07-27 15:55:46 +0800

    refact: test/draw use gtest fixture

---
+++ test/draw/draw.hpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "common/configs.h"
+#include "top/rvgpu.h"
+
+#define SHADER_BINARY_SIZE 0x1000
+
+class Draw : public ::testing::Test {
+protected:
+    void SetUp() override {
+        gpu = new rvgpu();
+    }
+
+    void TearDown() override {
+    }
+
+    void vertex_command(command_stream_vs cs_vs) {
+        commands.push_back(command_stream {.type = CS_TYPE_VS, .cmd = {cs_vs}});
+    }
+
+    void end_command() {
+        commands.push_back(command_stream {.type = CS_TYPE_END, .cmd = {}});
+    }
+
+    void run() {
+        gpu->run((uint64_t)commands.data());
+    }
+
+    void load_shader(const char *fname, uint32_t *binary) {
+        std::string fpath;
+        fpath += PROJECT_TEST_DRAW_SHADER_DATA_PATH;
+        fpath += "/";
+        fpath += fname;
+        FILE *fp = fopen(fpath.c_str(), "r");
+        char line[200];
+
+        uint32_t addr;
+        uint32_t b0, b1, b2, b3;
+        while (fgets(line, 200, fp)) {
+            sscanf(line, "%x: %x %x %x %x", &addr, &b0, &b1, &b2, &b3);
+            uint32_t data = ((b0 & 0xff) << 0) |
+                            ((b1 & 0xff) << 8) |
+                            ((b2 & 0xff) << 16) |
+                            ((b3 & 0xff) << 24);
+            binary[addr/4] = data;
+            // printf("%x: %08x\n", addr, data);
+        }
+    }
+
+private:
+    rvgpu *gpu;
+    std::vector<command_stream> commands;
+};
--- test/draw/vertex.cpp
+++ test/draw/vertex.cpp
@@ -2,34 +2,32 @@
 
 #include "top/rvgpu.h"
 #include "common/command_stream.h"
-#include "read_asm.hpp"
 
-TEST(Draw, Draw_add) {
-    uint32_t binary[1000];
-    read_asm_file("add.vs", binary);
+#include "draw.hpp"
 
-    rvgpu rvgpu;
+TEST_F(Draw, vertex_shader_add) {
+    int i = 0;
+    uint32_t binary[SHADER_BINARY_SIZE];
 
+    load_shader("add.vs", binary);
     cs_vs_desc *vs = (cs_vs_desc *)malloc(sizeof(cs_vs_desc));
     vs->input = (uint64_t)malloc(100);
     vs->output = (uint64_t)malloc(100);
     vs->shader = (uint64_t)binary;
+    command_stream_vs cs_vs = {};
+    cs_vs.vertex_count = 16;
+    cs_vs.desc = (uint64_t)vs;
+
+    vertex_command(cs_vs);
+    end_command();
+
+    // input of data
     int *in = (int *)vs->input;
-    int i = 0;
     for (i=0; i<16; i++) {
         in[i] = i * 100;
     }
 
-    std::vector<command_stream> cs;
-    command_stream_vs cs_vs;
-    cs_vs.vertex_count = 16;
-    cs_vs.desc = (uint64_t)vs;
-
-
-    cs.push_back(command_stream {.type = CS_TYPE_VS, .cmd = {cs_vs}});
-    cs.push_back(command_stream {.type = CS_TYPE_END, .cmd = {}});
-
-    rvgpu.run((uint64_t)cs.data());
+    run();
 
     int *out = (int *)vs->output;
     for (i=0; i<16; i++) {
