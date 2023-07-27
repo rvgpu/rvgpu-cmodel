59fab5f32ee65b3bbcb8f6d1effe661f3ddf3b9d
Qi Hangkong <qihangkong@outlook.com>
2023-07-27 17:29:20 +0800

    test: add a test of multi_array_mul

---
+++ test/draw/data/multi_array_mul.c
@@ -0,0 +1,25 @@
+typedef struct {
+    unsigned long input1;
+    unsigned long input2;
+    unsigned long output;
+} io;
+
+long vs_main(long layout, long vid)
+{
+    int *input1;
+    int *input2;
+    int *output;
+
+    // Global pointer 
+    io *desc = (io *)layout;
+    input1 = (int *)desc->input1;
+    input2 = (int *)desc->input2;
+    output = (int *)desc->output;
+
+    int a = input1[vid];
+    int b = input2[vid];
+    int out = a * b;
+    output[vid] = out;
+
+    return 0;
+}
--- test/draw/vertex.cpp
+++ test/draw/vertex.cpp
@@ -76,3 +76,41 @@ TEST_F(Draw, vertex_shader_multi_array_add) {
         EXPECT_EQ(out[i], in1[i] + in2[i]);
     }
 }
+
+TEST_F(Draw, vertex_shader_multi_array_mul) {
+    int i = 0;
+    uint32_t binary[SHADER_BINARY_SIZE];
+    load_shader("multi_array_mul.vs", binary);
+
+    struct addlayout {
+        uint64_t input1;
+        uint64_t input2;
+        uint64_t output;
+    };
+    struct addlayout *layout = (struct addlayout *)malloc(sizeof(struct addlayout));
+    layout->input1 = (uint64_t)malloc(100);
+    layout->input2 = (uint64_t)malloc(100);
+    layout->output = (uint64_t)malloc(100);
+    rvgpu_command_vs cs_vs = {};
+    cs_vs.vertex_count = 32;
+    cs_vs.layout = (uint64_t)layout;
+    cs_vs.shader = (uint64_t)binary;
+
+    vertex_command(cs_vs);
+    end_command();
+
+    // input of data
+    int *in1 = (int *)layout->input1;
+    int *in2 = (int *)layout->input2;
+    for (i=0; i<32; i++) {
+        in1[i] = i * 100;
+        in2[i] = i * 43;
+    }
+
+    run();
+
+    int *out = (int *)layout->output;
+    for (i=0; i<16; i++) {
+        EXPECT_EQ(out[i], in1[i] * in2[i]);
+    }
+}
