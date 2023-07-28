33a9aabd90d14d83672c4516379b8f18f7320d91
Qi Hangkong <qihangkong@outlook.com>
2023-07-28 18:34:30 +0800

    test: updata clang params to compile c testcase

--- test/draw/data/build.sh
+++ test/draw/data/build.sh
@@ -3,8 +3,10 @@
 
 fname=`echo "${1%.*}"`
 
-~/git/rvgpu/install/bin/clang -emit-llvm -c $1 -S -o test.ll -target riscv64 -mcpu="rvgpu" -fno-pic -fno-omit-frame-pointer -mno-save-restore -fno-common -mcmodel=medany
+# ~/git/rvgpu/install/bin/clang -emit-llvm -c $1 -S -o test.ll -target riscv64 -mcpu="rvgpu" -fno-pic -fno-omit-frame-pointer -mno-save-restore -fno-common -mcmodel=medany
+~/git/rvgpu/install/bin/clang -emit-llvm -c $1 -S -o test.ll -target riscv64 -mcpu="rvgpu" -mcmodel=medlow -mno-relax
 ~/git/rvgpu/install/bin/llc test.ll -mcpu="rvgpu" -O3 --filetype=obj
 ~/git/rvgpu/install/bin/llvm-objdump -d test.o > ${fname}.vs
+
 
 rm -f test.o test.ll
