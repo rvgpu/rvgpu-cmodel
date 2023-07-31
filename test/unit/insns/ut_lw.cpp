6da435667050d56bdebafb9ba06cdb6e26324788
lewis <lewis@sietium.com>
2023-07-31 19:37:27 +0800

    feat: add instruction lw

--- test/unit/insns/ut_lw.cpp
+++ test/unit/insns/ut_lw.cpp
@@ -3,18 +3,17 @@
 
 #include "ut_insns.hpp"
 
-TEST_F(ut_rv64_insns, decode_and_execute_rv64i_lw) {
+TEST_F(ut_insns, decode_and_execute_rv64i_lw) {
     // 0x00c92503 : lw a0, 12(s2)
-
-    // Base = addr, offset = 12 / 4 = 3
-    int32_t *addr = (int32_t *) malloc(4 * sizeof(int32_t));
-    addr[3] = 1234;
-
     insts.push_back(0x00c92503);
-    LoadInst();
-    WRITE_REG(fetch.insn.rs1(), reinterpret_cast<uint64_t>(addr));
-    ExecuateInst();
-    uint64_t res = READ_REG(fetch.insn.rd());
+    
+    uint64_t *addr = (uint64_t *)(GetStackPointer() + 12);
+    *addr = 0x10001000;
 
-    EXPECT_EQ(res, 1234);
+    auto regr = reg::a0;
+    uint64_t result = 0;
+    SetIReg(reg::s2, GetStackPointer());
+    ExecuateInst();
+    result = GetIReg(regr);
+    EXPECT_EQ(result, 0x10001000);
 }
