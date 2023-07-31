92b51bbd1b904f413cfa71b47659911b2876c356
Qi Hangkong <qihangkong@outlook.com>
2023-07-31 11:38:33 +0800

    test: add ut_slli

--- test/unit/insns/ut_slli.cpp
+++ test/unit/insns/ut_slli.cpp
@@ -2,12 +2,12 @@
 #include "sm/compute_unit.h"
 #include "ut_insns.hpp"
 
-TEST_F(ut_rv64_insns, decode_and_execute_rv64i_slli){
+TEST_F(ut_insns, decode_and_execute_rv64i_slli){
     //bits: 0x02061613  #slli a2, a2, 32
     insts.push_back(0x02061613);
-    LoadInst();
-    WRITE_REG(fetch.insn.rs1(), 0x80000000);
+    SetIReg(reg::a2, 0x80000000);
     ExecuateInst();
-    auto res = READ_REG(fetch.insn.rd());
+
+    auto res = GetIReg(reg::a2);
     EXPECT_EQ(res, ((uint64_t)0x80000000) << 32);
 }
