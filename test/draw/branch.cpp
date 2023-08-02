67403fd86ecb9cc05f4c1cf0ea2ca130abcc3dd5
Qi Hangkong <qihangkong@outlook.com>
2023-08-02 12:20:17 +0800

    test: add a test of ifelse

--- test/draw/branch.cpp
+++ test/draw/branch.cpp
@@ -33,3 +33,33 @@ TEST_F(Draw, branch_if) {
         }
     }
 }
+
+TEST_F(Draw, branch_ifelse) {
+    Shader shader;
+    int32_t count = 32;
+    int i = 0;
+    uint32_t *in = (uint32_t *)malloc(count * sizeof(int));
+    uint32_t *out = (uint32_t *)malloc(count * sizeof(int));
+
+    shader.SetupShaderBinary("branch_ifelse.vs");
+    shader.SetupIO(0, (uint64_t)in);
+    shader.SetupIO(1, (uint64_t)out);
+
+    for (i=0; i<count; i++) {
+        in[i] = i * 100;
+        out[i] = 5678;   // test of array[i] + 100;
+    }
+
+    vertex_command(count, (uint64_t)shader.binary, (uint64_t)shader.layout);
+    end_command();
+
+    run();
+
+    for (i=0; i<count; i++) {
+        if (i%2 == 0) {
+            EXPECT_EQ(out[i], in[i] + 100);
+        } else {
+            EXPECT_EQ(out[i], in[i] + 200);
+        }
+    }
+}
---
+++ test/draw/data/branch_ifelse.c
@@ -0,0 +1,27 @@
+typedef struct {
+    unsigned long input;
+    unsigned long output;
+} io;
+
+long vs_main(long layout, long vid)
+{
+    int *input;
+    int *output;
+
+    // Global pointer 
+    io *desc = (io *)layout;
+    input = (int *)desc->input;
+    output = (int *)desc->output;
+
+    int a = input[vid];
+    int out = a;
+    if (vid % 2 == 0) {
+        out = out + 100;
+    } else {
+        out = out + 200;
+    }
+
+    output[vid] = out;
+
+    return 0;
+}
