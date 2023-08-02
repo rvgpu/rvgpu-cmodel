614d9119783e73854f0b72fd1688e4d88ec3b30d
Qi Hangkong <qihangkong@outlook.com>
2023-08-02 14:31:56 +0800

    test: add testbench of branch-if-if

--- test/draw/branch.cpp
+++ test/draw/branch.cpp
@@ -63,3 +63,37 @@ TEST_F(Draw, branch_ifelse) {
         }
     }
 }
+
+TEST_F(Draw, branch_ifif) {
+    Shader shader;
+    int32_t count = 32;
+    int i = 0;
+    uint32_t *in = (uint32_t *)malloc(count * sizeof(int));
+    uint32_t *out = (uint32_t *)malloc(count * sizeof(int));
+
+    shader.SetupShaderBinary("branch_ifif.vs");
+    shader.SetupIO(0, (uint64_t)in);
+    shader.SetupIO(1, (uint64_t)out);
+
+    for (i=0; i<count; i++) {
+        in[i] = i * 100;
+        out[i] = 5678;   // test of array[i] + 100;
+    }
+
+    vertex_command(count, (uint64_t)shader.binary, (uint64_t)shader.layout);
+    end_command();
+
+    run();
+
+    for (i=0; i<count; i++) {
+        if (i%2 == 0) {
+            if (i%4 == 0) {
+                EXPECT_EQ(out[i], in[i] + 400);
+            } else {
+                EXPECT_EQ(out[i], in[i] + 200);
+            }
+        } else {
+            EXPECT_EQ(out[i], in[i] + 100);
+        }
+    }
+}
