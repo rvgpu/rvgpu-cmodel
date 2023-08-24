d4b80cf7ebd511c5cbebf2012d57102fb9f0dedf
Qi Hangkong <qihangkong@outlook.com>
2023-08-24 09:30:33 +0800

    test: add test of trigonometric function

---
+++ test/draw/matrix.cpp
@@ -0,0 +1,20 @@
+#include "gpu_execuator.hpp"
+#include "data/matrix_model.cpp"
+
+#include <eigen3/Eigen/Eigen>
+
+TEST_F(GPUExecuator, vertex_shader_vt14) {
+    float *data = (float *)malloc(sizeof(float) * 16);
+    float *result = (float *)malloc(sizeof(float) * 16);
+
+    LoadELF("matrix_model");
+    PushParam(0); // tid
+    PushParam((uint64_t)data);
+    run1d(1);
+
+    gpumain(0, result);
+
+    for (uint32_t i=0; i<16; i++) {
+        EXPECT_FLOAT_EQ(data[i], result[i]);
+    }
+}
