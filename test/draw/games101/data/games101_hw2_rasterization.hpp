d0268537e14899ddb69e4392309107c39c067a9f
lewis <lewis@sietium.com>
2023-09-05 15:30:56 +0800

    fix: use simpler rasterization in hw2

--- test/draw/games101/data/games101_hw2_rasterization.hpp
+++ test/draw/games101/data/games101_hw2_rasterization.hpp
@@ -61,35 +61,12 @@ void rasterization(
         // We use the convention 0 > n > f in projection, so nearer objects have larger z
         if (z > out_depth_buffer[(pixel_y * WIDTH) + pixel_x]) {
             out_depth_buffer[(pixel_y * WIDTH) + pixel_x] = z;
-
-            float v0_r = t.color[0][0];
-            float v0_g = t.color[0][1];
-            float v0_b = t.color[0][2];
-            float v1_r = t.color[1][0];
-            float v1_g = t.color[1][1];
-            float v1_b = t.color[1][2];
-            float v2_r = t.color[2][0];
-            float v2_g = t.color[2][1];
-            float v2_b = t.color[2][2];
-
-            // Since attributes (such as color) have not gone through perspective projection, we use perspective-corrected interpolation here
-            float v0_w = t.v[0][3];
-            float v1_w = t.v[1][3];
-            float v2_w = t.v[2][3];            
-            float a = bary0 / v0_w;
-            float b = bary1 / v1_w;
-            float c = bary2 / v2_w;
-            bary0 = a / (a + b + c);
-            bary1 = b / (a + b + c);
-            bary2 = c / (a + b + c);
-
-            float color_r = bary0 * v0_r + bary1 * v1_r + bary2 * v2_r;
-            float color_g = bary0 * v0_g + bary1 * v1_g + bary2 * v2_g;
-            float color_b = bary0 * v0_b + bary1 * v1_b + bary2 * v2_b;
             
-            out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 0] = (uint8_t)color_r;
-            out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 1] = (uint8_t)color_g;
-            out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 2] = (uint8_t)color_b;
+            Eigen::Vector3f color = bary0 * t.color[0] + bary1 * t.color[1] + bary2 * t.color[2];
+
+            out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 0] = (uint8_t)color[0];
+            out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 1] = (uint8_t)color[1];
+            out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 2] = (uint8_t)color[2];
             out_color_buffer[(pixel_y * WIDTH + pixel_x) * 4 + 3] = 255;
         }
     }
