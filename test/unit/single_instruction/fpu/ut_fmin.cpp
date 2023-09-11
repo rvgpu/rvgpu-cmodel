c990b3c58cd5019149147d399b52c407c45e3e75
Qi Hangkong <qihangkong@outlook.com>
2023-09-11 15:19:37 +0800

    fix: fixed binary of fmin.s

--- test/unit/single_instruction/fpu/ut_fmin.cpp
+++ test/unit/single_instruction/fpu/ut_fmin.cpp
@@ -11,5 +11,5 @@ TEST_F(ut_inst, decode_and_execute_rv64if_fmin_s) {
     // 0x28100053 : fmin.s f0, f0, f1
     float a = 1.1f;
     float b = 2.1f;
-    test_instruction(0x2a100053, IN(reg::f0, a), IN(reg::f1, b), RES(reg::f0, a < b ? a : b));
+    test_instruction(0x28100053, IN(reg::f0, a), IN(reg::f1, b), RES(reg::f0, a < b ? a : b));
 }
