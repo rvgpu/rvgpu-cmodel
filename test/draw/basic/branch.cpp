a33f1f4c5366a226187bc7a0a949a6ca414e3ac4
lewis <lewis@sietium.com>
2023-09-22 11:55:36 +0800

    test: add test case branch_if_with_vram

--- test/draw/basic/branch.cpp
+++ test/draw/basic/branch.cpp
@@ -1,4 +1,39 @@
 #include "gpu_execuator.hpp"
+#include "gpu_execuator_with_vram.hpp"
+
+TEST_F(GPUExecuator_with_vram, branch_if_with_vram) {
+    uint32_t count = 32;
+    int i = 0;
+
+    // Data
+    uint64_t in = (uint64_t) gpu_malloc(count * sizeof(int));
+    uint64_t out = (uint64_t) gpu_malloc(count * sizeof(int));
+    for (i=0; i<count; i++) {
+        gpu_write_vram(in + i * sizeof(int), i * 100, sizeof(int));
+    }
+
+    // Shader
+    uint64_t shader_addr = (uint64_t) gpu_malloc(0x2000000 * sizeof(uint32_t));
+    LoadELF_with_vram(shader_addr, "basic", "branch_if");
+
+    // Parameters
+    uint64_t params_addr = (uint64_t) gpu_malloc(3 * sizeof(uint64_t));
+    PushParam_with_vram(params_addr, 0, 0);
+    PushParam_with_vram(params_addr, 1, (uint64_t)in);
+    PushParam_with_vram(params_addr, 2, (uint64_t)out);
+
+    run1d_with_vram(count, params_addr, 3);
+
+    for (i=0; i<count; i++) {
+        if (i%2 == 0) {
+            int result = gpu_read_vram(out + i * sizeof(int), sizeof(int));
+            EXPECT_EQ(result, (i * 100) + 100);
+        } else {
+            int result = gpu_read_vram(out + i * sizeof(int), sizeof(int));
+            EXPECT_EQ(result, (i * 100));
+        }
+    }
+}
 
 TEST_F(GPUExecuator, branch_if) {
     int32_t count = 32;
